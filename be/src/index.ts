f3dd8e3bc93baca39b9c301f25ce29b8c548e110
Alan Huang <alans40609@gmail.com>
2024-07-24 15:07:49 +0800

    update server port

--- be/src/index.ts
+++ be/src/index.ts
@@ -5,7 +5,7 @@ import { corsMiddleware } from './middlewares/cors.middleware';
 
 dotenv.config();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 
 const app = express();
 
