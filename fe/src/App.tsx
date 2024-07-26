8a456dd8cab96dd469deb9688e2955b107bf55c0
Alan Huang <alans40609@gmail.com>
2024-07-26 10:51:06 +0800

    add loading in patient table

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -15,54 +15,65 @@ import TextField from '@mui/material/TextField';
 import OrdersDialog from './components/PatientDialog';
 import Button from '@mui/material/Button';
 import SearchIcon from '@mui/icons-material/Search';
+import { CircularProgress } from '@mui/material';
 // import AddIcon from '@mui/icons-material/Add';
 
 function App() {
   const searchInputRef = useRef<HTMLInputElement | null>(null);
 
   const [patients, setPatients] = useState<IPatient[]>([]);
-  const [pagination, setPagination] = useState({ page: 0, rowsPerPage: 10, rowsPerPageOptions: [1, 5, 10, 25] });
+  const [pagination, setPagination] = useState({
+    page: 0,
+    rowsPerPage: 10,
+    rowsPerPageOptions: [1, 5, 10, 25],
+  });
   const [viewPatient, setViewPatient] = useState<IPatient>();
+  const [isLoading, setIsLoading] = useState(false);
 
-  const columns: TableCellProps[] = [
-    { id: 'name', children: 'Name' },
-  ]
+  const columns: TableCellProps[] = [{ id: 'name', children: 'Name' }];
 
-  const showPatients = patients.slice(pagination.page * pagination.rowsPerPage, pagination.page * pagination.rowsPerPage + pagination.rowsPerPage);
+  const showPatients = patients.slice(
+    pagination.page * pagination.rowsPerPage,
+    pagination.page * pagination.rowsPerPage + pagination.rowsPerPage
+  );
 
-  const getPatients = useCallback(
-    async () => {
-      if (!searchInputRef.current) return;
-      const res = await PatientApi.getPatients(searchInputRef.current.value);
-      if (res.success) {
-        setPatients(res.data ?? []);
-      } else {
-        console.error(res.error);
-      }
-    },
-    [],
-  )
+  const getPatients = useCallback(async () => {
+    if (!searchInputRef.current) return;
+    setIsLoading(true);
+    const res = await PatientApi.getPatients(searchInputRef.current.value);
+    if (res.success) {
+      setPatients(res.data ?? []);
+    } else {
+      console.error(res.error);
+    }
+    setIsLoading(false);
+  }, []);
 
   useEffect(() => {
     getPatients();
-  }, [getPatients])
+  }, [getPatients]);
 
   const handleSearchPatient = () => {
     getPatients();
-  }
+  };
 
   const handleClickEditButton = (patient: IPatient) => {
     setViewPatient(patient);
-  }
+  };
 
-  const handlePageChange = (_: React.MouseEvent<HTMLButtonElement> | null, page: number) => {
+  const handlePageChange = (
+    _: React.MouseEvent<HTMLButtonElement> | null,
+    page: number
+  ) => {
     setPagination({ ...pagination, page });
-  }
+  };
 
-  const handleRowsPerPageChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+  const handleRowsPerPageChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
     const rowsPerPage = parseInt(e.target.value);
     setPagination({ ...pagination, rowsPerPage });
-  }
+  };
 
   return (
     <Box>
@@ -74,35 +85,57 @@ function App() {
           placeholder='請輸入住民的姓名'
           InputProps={{
             startAdornment: (
-              <InputAdornment position="start">
+              <InputAdornment position='start'>
                 <SearchIcon />
               </InputAdornment>
             ),
           }}
-          onKeyDown={e => e.key === 'Enter' && handleSearchPatient()}
+          onKeyDown={(e) => e.key === 'Enter' && handleSearchPatient()}
         />
-        <Button variant='contained' startIcon={<SearchIcon />} onClick={handleSearchPatient}>搜尋</Button>
+        <Button
+          variant='contained'
+          startIcon={<SearchIcon />}
+          onClick={handleSearchPatient}
+        >
+          搜尋
+        </Button>
         {/* <Button variant='outlined' startIcon={<AddIcon />}>新增住民</Button> */}
       </Box>
       <Paper sx={{ height: '80vh', display: 'flex', flexDirection: 'column' }}>
+        {isLoading && (
+          <CircularProgress
+            sx={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+            }}
+          />
+        )}
         <TableContainer sx={{ flexGrow: 1, overflow: 'auto' }}>
           <Table stickyHeader>
             <TableHead>
               <TableRow>
-                {columns.map(c => <TableCell key={c.id} {...c}>{c.children}</TableCell>)}
+                {columns.map((c) => (
+                  <TableCell key={c.id} {...c}>
+                    {c.children}
+                  </TableCell>
+                ))}
               </TableRow>
             </TableHead>
             <TableBody>
-              {showPatients.map(p => (
+              {showPatients.map((p) => (
                 <TableRow key={p.id} hover sx={{ cursor: 'pointer' }}>
-                  <TableCell onClick={() => handleClickEditButton(p)}>{p.name}</TableCell>
+                  <TableCell onClick={() => handleClickEditButton(p)}>
+                    {p.name}
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
         <TablePagination
-          component="div"
+          component='div'
           count={patients.length}
           page={pagination.page}
           rowsPerPage={pagination.rowsPerPage}
@@ -111,13 +144,17 @@ function App() {
           onRowsPerPageChange={handleRowsPerPageChange}
           labelRowsPerPage='每頁顯示'
           labelDisplayedRows={({ from, to, count }) =>
-            `${from} - ${to} 共 ${count !== -1 ? count : `超過 ${to}`} 筆`}
-        >
-        </TablePagination>
+            `${from} - ${to} 共 ${count !== -1 ? count : `超過 ${to}`} 筆`
+          }
+        ></TablePagination>
       </Paper>
-      <OrdersDialog viewPatient={viewPatient} setViewPatient={setViewPatient} getPatients={getPatients} />
+      <OrdersDialog
+        viewPatient={viewPatient}
+        setViewPatient={setViewPatient}
+        getPatients={getPatients}
+      />
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
