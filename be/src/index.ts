b496b0ef74d9be58db6814ab726c62003274ddef
Alan Huang <alans40609@gmail.com>
2024-07-26 10:39:02 +0800

    health check and errorhandler

--- be/src/index.ts
+++ be/src/index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express, { json, urlencoded } from 'express';
 import { apiRouter } from './routes';
 import { corsMiddleware } from './middlewares/cors.middleware';
+import { errorMiddleware } from './middlewares/error.middleware';
 
 dotenv.config();
 
@@ -13,7 +14,17 @@ app
   .use(corsMiddleware)
   .use(json())
   .use(urlencoded({ extended: true }))
-  .use('/api', apiRouter);
+  // Health Check
+  .get('/healthz', async (_, res, next) => {
+    try {
+      res.send(new Date().toISOString() + ' health check');
+    } catch (error) {
+      console.error(`Health Check Error: ${error}`);
+      next(error);
+    }
+  })
+  .use('/api', apiRouter)
+  .use(errorMiddleware);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
